391f93ea60b0ad724c7f6ebab8b6b2e9af566c51
Ashley Wong <tofudragon333@gmail.com>
2022-06-29 17:23:56 -0400

    commit tree 9b79d91c7f1047363ca1731505f88ee77a7d73d7

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+    #setter
+    def name=(value)
+        @name = value
+    end
 
+    #getter
+    def name
+        @name
+    end
+
+    #setter
+    def breed=(value)
+        @breed = value
+    end
+
+    def breed
+        @breed
+    end
 end
